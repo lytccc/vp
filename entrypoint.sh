980e828841e99be0275197b4d86670b7881653df
lytccc <lytccc@users.noreply.github.com>
2021-11-06 19:25:02 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -L -H "Cache-Control: no-cache" -o /tmp/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip &&
-  unzip /tmp/v.zip -d /usr/bin &&
-  rm -rf /tmp/v.zip
-v2ray -config /config.json
